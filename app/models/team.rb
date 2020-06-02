856c99ef01397782db139095fc8e8bd1460a1758
lmonty22 <lindsay.montgomery@olympian.org>
2020-06-02 17:57:43 -0400

    day one backend

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,3 @@
 class Team < ApplicationRecord
-    has_many :match_ups
+    # has_many :match_ups
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :teams
-  resources :match_ups
-  resources :rounds
-  resources :tournaments
+  # resources :teams
+  # resources :match_ups
+  # resources :rounds
+  resources :tournaments, only: [:create, :show, :index, :update, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
