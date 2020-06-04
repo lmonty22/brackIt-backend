bc4ca0da383cee1116e63b61c9ae57b348ddbef6
lmonty22 <lindsay.montgomery@olympian.org>
2020-06-03 21:33:44 -0400

    started updating matchups feature. very buggy

--- app/controllers/match_ups_controller.rb
+++ app/controllers/match_ups_controller.rb
@@ -1,2 +1,28 @@
 class MatchUpsController < ApplicationController
+    def update
+        #update round that was clicked on 
+        mc = MatchUp.find(params[:id])
+        mc.update(match_up_params)
+        tournament = mc.round.tournament
+
+        #this is the second round we need to update 
+        level_up_round = tournament.rounds.find_by(round_number: (mc.round.round_number+1))
+        level_up_match_up_number = mc.match_up_number.odd?? (mc.match_up_number + 1)/2 : mc.match_up_number/2 
+        level_up_match_up = level_up_round.match_ups.find {|m|  m.match_up_number == level_up_match_up_number}
+
+            if mc.match_up_number.odd?
+                level_up_match_up.update(team_a_id: mc.winner_id)
+            else 
+                level_up_match_up.update(team_b_id: mc.winner_id)
+            end
+        render :json => tournament.as_json(include: {rounds: {
+            include: {match_ups: {
+                include: [:team_a, :team_b]
+            } } }})
+    end
+
+    private
+    def match_up_params
+        params.require(:match_up).permit(:team_a_id, :team_b_id, :winner_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # resources :teams
-  # resources :match_ups
+  resources :match_ups, only: [:update]
   # resources :rounds
   resources :tournaments, only: [:create, :show, :index, :update, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
