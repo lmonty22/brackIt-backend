79462a4e61f9ca83946786c7762493ae9186d0db
lmonty22 <lindsay.montgomery@olympian.org>
2020-06-04 12:52:09 -0400

    back end updating matchup next level... a little glitchy... almost works

--- app/controllers/match_ups_controller.rb
+++ app/controllers/match_ups_controller.rb
@@ -4,17 +4,17 @@ class MatchUpsController < ApplicationController
         mc = MatchUp.find(params[:id])
         mc.update(match_up_params)
         tournament = mc.round.tournament
-
         #this is the second round we need to update 
-        level_up_round = tournament.rounds.find_by(round_number: (mc.round.round_number+1))
-        level_up_match_up_number = mc.match_up_number.odd?? (mc.match_up_number + 1)/2 : mc.match_up_number/2 
-        level_up_match_up = level_up_round.match_ups.find {|m|  m.match_up_number == level_up_match_up_number}
-
-            if mc.match_up_number.odd?
-                level_up_match_up.update(team_a_id: mc.winner_id)
-            else 
-                level_up_match_up.update(team_b_id: mc.winner_id)
-            end
+        next_round = tournament.rounds.find_by(round_number: (mc.round.round_number+1))
+        if mc.match_up_number.odd?
+            nr_mc_num = (mc.match_up_number + 1)/ 2
+            next_match_up = next_round.match_ups.find_by(match_up_number: nr_mc_num)
+            next_match_up.update(team_a_id: mc.winner_id)
+        else 
+            nr_mc_num = (mc.match_up_number/2)
+            next_match_up = next_round.match_ups.find_by(match_up_number: nr_mc_num)
+            next_match_up.update(team_b_id: mc.winner_id)
+        end
         render :json => tournament.as_json(include: {rounds: {
             include: {match_ups: {
                 include: [:team_a, :team_b]
