859dbc506f5a7ad1f1145972bea8cc8f8f5cf110
lmonty22 <lindsay.montgomery@olympian.org>
2020-06-05 17:30:17 -0400

    fixed post first round matchups

--- app/models/round.rb
+++ app/models/round.rb
@@ -5,16 +5,16 @@ class Round < ApplicationRecord
     def create_match_ups(teams)
         number_of_match_ups = teams.length/self.round_number/2
         x = 1
-        number_of_match_ups.times do 
+        (number_of_match_ups.to_i).times do 
             mc = MatchUp.create(round_id: self.id, match_up_number: x)
             x += 1
         end
+        teams2 = teams.clone
         if self.round_number == 1  
             self.match_ups.each do |m|
-                team_a = teams.shift
-                m.team_a_id = team_a.id
-                team_b = teams.shift
-                m.team_b_id = team_b.id
+                team_a = teams2.shift
+                team_b = teams2.shift
+                m.update(team_a_id: team_a.id, team_b_id:team_b.id)
             end
         end
     end
