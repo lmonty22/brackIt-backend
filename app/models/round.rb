4de103db6f869465d1a64a92e850e39d096f576f
lmonty22 <lindsay.montgomery@olympian.org>
2020-06-06 18:28:35 -0400

    dont know if i chnanged anything

--- app/models/round.rb
+++ app/models/round.rb
@@ -4,7 +4,6 @@ class Round < ApplicationRecord
     
     def create_match_ups(teams)
         number_of_match_ups = teams.length/(2**self.round_number)
-        byebug
         x = 1
         number_of_match_ups.times do 
             mc = MatchUp.create(round_id: self.id, match_up_number: x)
@@ -13,7 +12,6 @@ class Round < ApplicationRecord
         teams2 = teams.map{|t| t.id}
         if self.round_number == 1  
             self.match_ups.each do |m|
-                byebug
                 team_a = teams2.shift
                 team_b = teams2.shift
                 m.update(team_a_id: team_a, team_b_id:team_b)
