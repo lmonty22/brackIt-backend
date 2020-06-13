34f0618b215a5978e3c5284eb0345162cda09389
lmonty22 <lindsay.montgomery@olympian.org>
2020-06-13 11:39:55 -0400

    refactor store

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -10,22 +10,21 @@ class TournamentsController < ApplicationController
 
     def index
         t = Tournament.all 
-        render :json => t.as_json(include: [:champion, :user, {rounds: {
-                                              include: {match_ups: {
-                                                  include: [:team_a, :team_b]
-                                              } } }
-        }])
+        render :json => t.as_json(include: [:champion, :user])
     end
 
     def create
         t = Tournament.create(name: params[:name], number_of_teams: params[:numberOfTeams], user_id: params[:user_id])
         teams = Team.create_teams(params[:numberOfTeams], params[:teamNames])
         t.create_rounds(teams)
-        render :json => t.as_json(include: [:champion, :user, {rounds: {
-                                              include: {match_ups: {
-                                                  include: [:team_a, :team_b]
-                                              } } }
-        }])
+        render :json => t.as_json(include: [:champion, :user])
+            # maybe render this one if I force a redirect? 
+            
+        # render :json => t.as_json(include: [:champion, :user, {rounds: {
+        #                                       include: {match_ups: {
+        #                                           include: [:team_a, :team_b]
+        #                                       } } }
+        # }])
     end
 
     def destroy
