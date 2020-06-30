a53f9cd1ba149f59aaf353180b790f955fdae358
lmonty22 <lindsay.montgomery@olympian.org>
2020-06-30 16:34:34 -0400

    change cors temp

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://git.heroku.com/brackit22.git'
+    origins '*'
 
     resource '*',
       headers: :any,
