683b562d85e35bc9aec105188c9168096bee5d4e
lmonty22 <lindsay.montgomery@olympian.org>
2020-06-30 15:29:21 -0400

    typo in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://brackit22.herokuapp.com/'
+    origins 'https://git.heroku.com/brackit22.git'
 
     resource '*',
       headers: :any,
