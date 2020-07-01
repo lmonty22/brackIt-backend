3d901f090f6f691d28bc1a5cdcbaf64c876d509b
lmonty22 <lindsay.montgomery@olympian.org>
2020-07-01 14:45:29 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://brackit.netlify.app/'
+    origins 'https://brackit.netlify.app', 'https://5efcd8b40bb2006a85ab1abe--brackit.netlify.app'
 
     resource '*',
       headers: :any,
